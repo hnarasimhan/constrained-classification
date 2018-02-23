c5f7e8b7a963aa51da7ed3c87ea84a72089d6a84
hnarasimhan <hnarasimhan@g.harvard.edu>
2018-02-23 18:03:19 -0500

    delete old folder

--- complex_performance_metrics_copy/README.md
+++
@@ -1,1 +0,0 @@
-# constrained-classification
--- complex_performance_metrics_copy/solvers/__init__.py
+++
@@ -1,2 +0,0 @@
-# Map from performance measure to constraint functions
-PERF_CONS_MAP = {'hmean': 'cov', 'qmean': 'dp', 'fmeasure': 'kld', 'microF1':'cov', 'err': 'dp'}
