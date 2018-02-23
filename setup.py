15f5fe197eb760cb339bded1bed82b0acdd12469
hnarasimhan <hnarasimhan@g.harvard.edu>
2018-02-23 16:43:24 -0500

    Removed batch.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='constrained_classification',
+    version='',
+    packages=['', 'constrained_classification', 'constrained_classification.models',
+              'constrained_classification.solvers', 'constrained_classification.solvers.binary',
+              'constrained_classification.solvers.multiclass'],
+    url='',
+    license='',
+    author='harikrishna',
+    author_email='',
+    description=''
+)
