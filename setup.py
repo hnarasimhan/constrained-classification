6e913e3e183cebc8ce017ddbdf051cec8bdaf61c
hnarasimhan <hnarasimhan@seas.harvard.edu>
2018-03-16 15:51:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='complex_performance_metrics',
-    version='',
-    packages=['', 'complex_performance_metrics', 'complex_performance_metrics.models',
+    version='1.0',
+    packages=['complex_performance_metrics', 'complex_performance_metrics.models',
               'complex_performance_metrics.solvers', 'complex_performance_metrics.solvers.binary',
               'complex_performance_metrics.solvers.multiclass'],
     url='',
-    license='',
-    author='harikrishna',
-    author_email='',
-    description=''
+    license='MIT Licence',
+    author='Harikrishna Narasimhan',
+    author_email='hnarasimhan@seas.harvard.edu',
+    description='Algorithms for learning with complex loss functions and constraints'
 )
